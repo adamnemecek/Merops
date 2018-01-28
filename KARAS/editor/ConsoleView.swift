0755826b9272392f4be1e71bf8434122c6dbe889
Narumi <sho7noka@users.noreply.github.com>
2018-01-28 13:44:54 +0900

    clean

---
+++ KARAS/editor/ConsoleView.swift
@@ -0,0 +1,9 @@
+//
+//  ConsoleView.swift
+//  KARAS
+//
+//  Created by sumioka-air on 2018/01/06.
+//  Copyright © 2018年 sho sumioka. All rights reserved.
+//
+
+import Foundation
--- KARAS/engine/USDTest.mm
+++
@@ -1,9 +0,0 @@
-//
-//  USDTest.cpp
-//  KARAS
-//
-//  Created by sumioka-air on 2017/08/21.
-//  Copyright © 2017年 sho sumioka. All rights reserved.
-//
-
-#include "USDTest.hpp"
